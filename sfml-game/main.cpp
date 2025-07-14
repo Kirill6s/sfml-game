c974f30c2f1f231ad8bb18705900b4c1e3e1fb3f
Кирилл Скрипников <Kirill.Skripnikow@yandex.ru>
2025-07-14 14:23:44 +0300

    SFML

--- sfml-game/main.cpp
+++ sfml-game/main.cpp
@@ -1,10 +1,29 @@
-#include <iostream>
-using namespace std;
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Clear screen
+        window.clear();
 
 
-int main() {
+        // Update the window
+        window.display();
+    }
 
-	cout << "hello";
-	return 0;
-
+    return EXIT_SUCCESS;
 }
